eee491b09e644aeba407f85fa7aa3f1567f2b81d
karadagg0 <104911016+karadagg0@users.noreply.github.com>
2022-10-31 16:43:11 +0300

    change image because of  collision in unity
    In-game collision checking and image changing in Unity

---
+++ image change after collision/image change after collision/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class Program : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite brokenImage;
+
+    int count = 0;
+
+    private void OnCollisionEnter2D(Collision2D target)
+    {
+        if (target.gameObject.tag == "Ball")
+        {
+            count++;
+
+
+            if (count == 1)
+            {
+                GetComponent<SpriteRenderer>().sprite = brokenImage;
+            }
+
+            else if (count == 2)
+            {
+                Destroy(gameObject);
+            }
+
+
+        }
+
+
+
+
+    }
+}
